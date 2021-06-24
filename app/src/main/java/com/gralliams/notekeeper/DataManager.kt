8bdd125a9be62db9e50332d89edc59b411a75b89
Grant Williams <greatgrant116@gmail.com>
2021-06-24 22:57:52 +0100

    Initialized courses

--- app/src/main/java/com/gralliams/notekeeper/DataManager.kt
+++ app/src/main/java/com/gralliams/notekeeper/DataManager.kt
@@ -1,8 +1,25 @@
 package com.gralliams.notekeeper
 
 class DataManager {
-    //Holds reference to courses in a HashMap
+    //Holds reference to courses in a HashMap using Id of type String
     var courses = HashMap<String, CourseInfo>()
     //holds reference to notes in An arrayList
     var notes = ArrayList<NoteInfo>()
+
+
+    private fun initializeCourses(){
+        //Initialize CourseInfo object
+        //Add course to course HashMap with key and value
+        var course = CourseInfo("EEE", "Fundamentals Of Electrical Engineering I")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("GNS", "Use Of English")
+        courses.set(course.courseId, course)
+
+        course = CourseInfo("CE", "Strength of materials")
+        courses.set(course.courseId, course )
+
+        course = CourseInfo("CCE", "Computer and communication engineering")
+        courses.set(course.courseId, course)
+    }
 }
