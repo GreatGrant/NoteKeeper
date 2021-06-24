6d0f93b46931a562850b5261ec3aac5e7c356f0d
Grant Williams <greatgrant116@gmail.com>
2021-06-24 22:31:02 +0100

    Created DataManager.kt for holding references to the data classes in data structures

---
+++ app/src/main/java/com/gralliams/notekeeper/DataManager.kt
@@ -0,0 +1,8 @@
+package com.gralliams.notekeeper
+
+class DataManager {
+    //Holds reference to courses in a HashMap
+    var courses = HashMap<String, CourseInfo>()
+    //holds reference to notes in An arrayList
+    var notes = ArrayList<NoteInfo>()
+}
--- app/src/main/java/com/gralliams/notekeeper/NoteKeeperData.kt
+++ app/src/main/java/com/gralliams/notekeeper/NoteKeeperData.kt
@@ -1,4 +1,4 @@
 package com.gralliams.notekeeper
 
-class NoteInfo(val courseId: String, val title: String)
-class CourseInfo(var course: CourseInfo, var title: String, var text: String)
+class CourseInfo(val courseId: String, val title: String)
+class NoteInfo(var course: CourseInfo, var title: String, var text: String)
